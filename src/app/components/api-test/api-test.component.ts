8dc6d99af3e97c5fe2ccce0330ee4f103ed37c41
mathebe <mathebemoseri@gmail.com>
2026-02-28 14:15:32 +0200

    feat: implement initial static frontend form for API validation

---
+++ src/app/components/api-test/api-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-test.component.html',
+  styleUrl: './api-test.component.css'
+})
+export class ApiTestComponent {
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ApiTestComponent } from '../components/api-test/api-test.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ApiTestComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
