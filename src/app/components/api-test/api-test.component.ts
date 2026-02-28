d644cb785b11d0aa8bb9f4fd777633e4ca08b974
mathebe <mathebemoseri@gmail.com>
2026-02-28 19:17:30 +0200

    feat: introduce UI placeholders for loading, error, and response

--- src/app/components/api-test/api-test.component.ts
+++ src/app/components/api-test/api-test.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-api-test',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './api-test.component.html',
   styleUrl: './api-test.component.css'
 })
@@ -12,8 +13,20 @@ export class ApiTestComponent {
 
   email: string = '';
   endpointUrl: string = '';
+  response: any = null; 
+  loading = false; 
+  error: string | null = null;
 
   onSubmit() {
   console.log('submit', this.email, this.endpointUrl);
-}
+
+  this.loading = true;
+  this.error = null;
+  this.response = null;
+
+  setTimeout(() => { 
+    this.loading = false;
+    this.response = { message: 'Fake success response' }; 
+    }, 1500
+  );}
 }
